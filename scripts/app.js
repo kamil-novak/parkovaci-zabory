da83faf78710387ae9014f9fae3e86d745635bb8
Kamil Novák <kamil.novak86@gmail.com>
2024-06-16 11:14:44 +0200

    Zprovozněny popisky pro update.

--- scripts/app.js
+++ scripts/app.js
@@ -284,9 +284,9 @@ require([
         let startVertex = null;
         let endVertex = null;
  
+        // Při vytváření nové geometrie
         editorWidget.on("sketch-create", function(evt) {
           const { toolEventInfo } = evt.detail;
-          let segment;
           
           // Po vložení nového vertexu
           if (toolEventInfo?.type === "vertex-add") {
@@ -302,41 +302,13 @@ require([
             let geometry = new Polyline({
               spatialReference: view.spatialReference,
               paths: [[startVertex, endVertex]],
-              
             }) 
 
             // Výpočet délky linie
             let length = Math.round(geometryEngine.planarLength(geometry, "meters"));
 
-            // Výpočet úhlu popisku
-            let calculateAngle = (startVertex, endVertex) => {
-              let angle;
-              angle = ((180/Math.PI)*Math.atan2(endVertex[0]-startVertex[0], endVertex[1]-startVertex[1]))
-              if (angle < 0) {
-                return(angle+360 + 90)
-              }
-              else {
-                return(angle - 90)
-              }
-            }
-
             // Vytvoření grafiky
-            segment =  new Graphic({
-              geometry: geometry.extent.center,
-              symbol: {
-                type: "text", 
-                color: "white",
-                text: `\u{200B} \n${length} m`, // Odsazení od pomyslné linie neviditelným znakem
-                font: {
-                  size: 20
-                  },
-                haloColor: "black",
-                haloSize: 1.5,
-                horizontalAlignment: "center",
-                verticalAlignment: "middle",
-                angle: calculateAngle(startVertex, endVertex),
-                }
-            });
+            let segment = createGraphic(geometry, length, startVertex, endVertex);
 
             // Vložení nové grafiky do mapy
             view.map.reorder(LabelLayer, 9999);
@@ -344,6 +316,36 @@ require([
             LabelLayer.add(segment);
            }
         });
+
+        // Při úpravě stávající geometrie
+        editorWidget.on("sketch-update", function(evt) {
+          if(evt.detail.state === "active" && evt.detail.tool === "reshape") {
+            LabelLayer.removeAll();
+            let polygonVertexes = evt.detail.graphics[0].geometry.rings[0];
+            polygonVertexes.forEach((vertex, index) => {
+              if (index + 1 < polygonVertexes.length) {
+                let startVertex =  vertex;
+                let endVertex =  polygonVertexes[index+1];
+
+                // Vytvoření liniové geometrie z vertexu a pozice myši
+                let geometry = new Polyline({
+                  spatialReference: view.spatialReference,
+                  paths: [[startVertex, endVertex]],
+                }) 
+
+                // Výpočet délky linie
+                let length = Math.round(geometryEngine.planarLength(geometry, "meters"));
+
+                // Vytvoření grafiky
+                let segment = createGraphic(geometry, length, startVertex, endVertex);
+
+                // Vložení nové grafiky do mapy
+                view.map.reorder(LabelLayer, 9999);
+                LabelLayer.add(segment);
+              }
+            })
+          }
+        })
 
         // Widgets positioning
         view.ui.add(locateWidget, "top-left", 0);
@@ -388,369 +390,36 @@ require([
     });
 
     // FUNCTIONS ---
-    // HTML
-    // Problem window
-    // Show window for adding problem point to map
-    let showAddProblemToMapWindow = () => {
-      addProblemContainer.prepend(problemWindowContainer);
-      addProblemBtn.style.display = "none";
-
-      sketchingState = true;
-    }
-
-    // Close window for adding problem point to map
-    let closeAddProblemToMapWindow = () => {
-      problemWindowContainer.remove(); // Remove window for adding point
-      addProblemBtn.style.display = "flex"; // Enable create button
-      resetSketchViewModel();
-      
-      sketchingState = false;
-    }
-
-    let changeMessageInProblemToMapWindow = (message, actionBar) => {
-      problemWindowBody.innerHTML = message;
-      problemWindowBody.append(actionBar);
-    }
-
-    // Problems form
-    // Show problems form
-    let showProblemFormContainer = () => {
-      overlayEl.classList.add("opened");
-    }
-    let closeProblemFormContainer = () => {
-      problemFormContainer.scrollTo(0,0);
-      overlayEl.classList.remove("opened");
-    }
-
-    // Category
-    let selectCategory = (categoryCardEl, category) => {
-      setValidationMessage(problemFormCategory, "valid", "check", `Zvolen typ závady: ${category.name}`)
-      categoryCardEl.setAttribute("selected", "");
-    }
-
-    let resetAllCardSelection = () => {
-      problemFormCategory.querySelectorAll("calcite-card").forEach((card) => {
-        card.removeAttribute("selected");
-      })
-    }
-
-    // Attachment
-    // Remove attachement
-    const removeAttachment = () => {
-      setValidationMessage(problemFormAttachment, "invalid", "exclamation-mark-triangle", messageInitialFormAttachment)
-      removeAttachmentBtn.style.display = "none";
-      attachmentFormEl.reset();
-    }
-
-    // DOM after image loaded
-    const afterAttachmentLoaded = (imageFile) => {
-      removeAttachmentBtn.style.display = "inline-block";
-      addInfoAboutImageToInput(imageFile);
-    }
-
-    // DOM after bad file loaded
-    const afterBadFileLoaded = () => {
-      setValidationMessage(problemFormAttachment, "invalid", "exclamation-mark-triangle", "Nepodporovaný soubor!")
-      removeAttachmentBtn.style.display = "none";
-    }
-
-    // Info about added image
-    const addInfoAboutImageToInput = (imageFile) => {
-      setValidationMessage(problemFormAttachment, "valid", "check", `${imageFile.name} (${formatFileSize(imageFile.size)})`)
-    }
-
-    // Validation message
-    let setValidationMessage = (el, status, icon, text) => {
-      let messageEl = el.querySelector("calcite-input-message");
-      messageEl.innerText = text;
-      messageEl.status = status;
-      messageEl.icon = icon;
-    }
-
-    // Loading over form
-    let addLoadingScreenOverForm = () => {
-      problemLoading.style.display = "block";
-    }
-    let removeLoadingScreenOverForm = () => {
-      problemLoading.style.display = "none";
-    }
-
-    // Result screen over form
-    let addResultScreenOverForm = (type) => {
-      problemResultScreen.style.display = "block";
-      if ( type === "success" ) {
-        problemResultScreen.innerHTML = problemSendedSuccess;
-      }
-      if ( type === "error" ) {
-        problemResultScreen.innerHTML = problemSendedError;
-      }
-    }
-    let removeResultScreenOverForm = () => {
-      problemResultScreen.style.display = "none";
-      problemResultScreen.innerHTML = null;
-    }
-
-    // Reset form inputs
-    let resetForm = () => {
-      resetAllCardSelection();
-      document.querySelectorAll(".problem-input").forEach((input) => {
-        input.value = null;
-      });
-
-      attachmentFormEl.reset();
-      removeAttachment();	
-
-      setValidationMessage(problemFormCategory, "invalid", "information", messageInitialFormCategory)
-      setValidationMessage(problemFormDescription, "invalid", "information", messageInitialFormDescription);
-      setValidationMessage(problemFormEmail, "invalid", "information", messageInitialFormEmail);
-      setValidationMessage(problemFormAttachment, "invalid", "information", messageInitialFormAttachment);
-    }
-    
-    // BUSINESS - MAIN
-    // Geometry
-    // Reset sketch view model
-    let resetSketchViewModel = () => {
-      changeMessageInProblemToMapWindow(messageSelectPlace, problemWindowLocateBtn); 
-      sketchLayer.graphics.removeAll(); // Remove graphic from map
-      sketchViewModel.cancel();
-      setState("geometry", null);
-    }
-
-    // Active sketching point problem in map
-    let activateSketchingToMap = () => {
-      // Create model
-      sketchViewModel = new SketchViewModel(sketchViewModelOptions);
-      // Initialize sketching
-      sketchViewModel.create("point");
-      // Move locate graphic under závada graphic
-      moveLocateGraphicUnderSketch()
-      // Events
-      sketchViewModel.on("create", function(e) {
-        sketchLayer.graphics.removeAll();
-        if(e.state === "complete") {
-          sketchLayer.graphics.add(e.graphic);
-          changeMessageInProblemToMapWindow(messageSelectPlaceSuccess, problemActionBar); 
-
-          setState("geometry", e.graphic);
-          //activateSketchingToMap(); OPTION: just click for geometry update
-        }
-      });
-      sketchViewModel.on("update", function(e) {
-        setState("geometry", e.graphics[0]);
-      });
-    }
-
-    // Geometry from location
-    let placeSketchToMapDirectly = (e) => {
-      sketchViewModel.cancel();
-      // Create map graphic
-      let graphic = new Graphic({
-        geometry: {
-          type: "point",
-          longitude: e.coords.longitude, 
-          latitude: e.coords.latitude
-        },
-        symbol: sketchSymbol
-      });
-      sketchLayer.graphics.add(graphic);
-      changeMessageInProblemToMapWindow(messageSelectPlaceSuccess, problemActionBar); 
-      
-      setState("geometry", graphic);
-    }
-
-    // Validate send form button
-    let validateSendButton = () => {
-      let valid = false;
-      for (let prop in formState) {
-        if (formState[prop] === null) {
-            valid = false;
-            break;
-        }
-        else {
-          valid = true;
-        }
-      }
-      valid ? problemSendBtn.removeAttribute("disabled") : problemSendBtn.setAttribute("disabled", "");
-    }
-
-    // Create feature for send 
-    let createFeatureForSend = () => {
-      let feature = formState.geometry;
-      feature.setAttribute("typ", formState.category);
-      feature.setAttribute("email", formState.email);
-      feature.setAttribute("poznamka", formState.description);
-      return(feature)
-    }
-
-    let resetApp = () => {
-      resetState();
-      resetForm();
-      closeAddProblemToMapWindow();
-      addEmailFromLocalStorage();  
-    }
-
-    // Set state
-    let setState = (type, value) => {
-      formState[type] = value;
-      validateSendButton();
-      console.log(`State update - ${type}: `, formState);
-    }
-
-    let resetState = () => {
-      for(let props in formState) {
-        formState[props] = null
-      }
-      console.log(`State reset: `, formState);
-    }
-
-    let addEmailFromLocalStorage = () => {
-      if (localStorage.getItem("hlaseni_zavad_email")) {
-        let localSotarageEmail = localStorage.getItem("hlaseni_zavad_email");
-        problemFormEmail.querySelector("calcite-input").value = localSotarageEmail;
-        setValidationMessage(problemFormEmail, "valid", "check", "e-mail vložen.");
-        setState("email", localSotarageEmail)
-      };
-    }
-
-    // Refresh layer with added feature
-    let refreshMapLayer = (layer) => {
-      layer.refresh();
-    }
-
-    // BUSINESS - OTHER
-    // Move locate graphic under závada graphic
-    let moveLocateGraphicUnderSketch = () => {
-      if (view.graphics) {
-        view.graphics.forEach(graphic => {
-          if (graphic.attributes) {
-            if ('altitudeAccuracy' in graphic.attributes) {
-              view.graphics.removeAll(); 
-              locateLayer.add(graphic);
-            }
+    // Vytvoření grafiky
+    let createGraphic = (geometry, length, startVertex, endVertex) => {
+      let segment =  new Graphic({
+        geometry: geometry.extent.center,
+        symbol: {
+          type: "text", 
+          color: "white",
+          text: `\u{200B} \n${length} m`, // Odsazení od pomyslné linie neviditelným znakem
+          font: {
+            size: 20
+            },
+          haloColor: "black",
+          haloSize: 1.5,
+          horizontalAlignment: "center",
+          verticalAlignment: "middle",
+          angle: calculateAngle(startVertex, endVertex),
           }
-        });
-      }
+      });
+      return(segment)
     }
 
-    // Email validation
-    let validateEmail = (email) => {
-      return String(email)
-        .toLowerCase()
-        .match(
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-    };
-
-    // Format file size
-    let formatFileSize = (size) => {
-      if (size < 1000000){
-        return(Math.floor(size/1000) + ' kB');
+    // Výpočet úhlu popisku
+    let calculateAngle = (startVertex, endVertex) => {
+      let angle;
+      angle = ((180/Math.PI)*Math.atan2(endVertex[0]-startVertex[0], endVertex[1]-startVertex[1]))
+      if (angle < 0) {
+        return(angle+360 + 90)
       }
       else {
-        return(Math.floor(size/1000000) + ' MB');  
+        return(angle - 90)
       }
-    } 
-
-    // Resize attachment
-    // Process attachment file
-    let processAttachmentFile = async (file) => {
-      
-      if( !( /image/i ).test( file.type ) ) {
-          return false;
-      }
-
-      // Read the files
-      let reader = new FileReader();
-      reader.readAsArrayBuffer(file);
-
-      let newform = document.createElement("form");
-      let newinput = document.createElement("input");
-      
-      reader.onload = function (event) {
-
-        // Blob stuff
-        let blob = new Blob([event.target.result]); // create blob...
-        window.URL = window.URL || window.webkitURL;
-        let blobURL = window.URL.createObjectURL(blob); // and get it's URL
-        
-        // Helper Image object
-        let image = new Image();
-        image.src = blobURL;
-
-        image.onload = function() {
-          // Have to wait till it's loaded
-          // Send it to canvas
-          let resized = resizeImage(image); 
-
-          // Convert the canvas content to a Blob
-          resized.toBlob((blob) => {
-
-            // Create a new File from the Blob
-            let resizedFile = new File([blob], file.name, { type: 'image/jpeg' });
-            let container = new DataTransfer();
-            container.items.add(resizedFile);
-                    
-            // Create new virtual form
-            newinput.setAttribute("type", "file"); 
-            newinput.setAttribute("name", "attachment"); 
-            newinput.setAttribute("accept", "image/*");
-            newinput.files = container.files;
-            newform.append(newinput);
-
-            afterAttachmentLoaded(resizedFile);
-
-            // Attachment data for custom esriRequest
-            // It can be removed after solving standard applyEdits() method
-            var attachmentData = new FileReader();
-            attachmentData.onload = function(event) {
-                let attachmentDataContent = event.target.result.replace(/^data:image\/[a-z]+;base64,/, "");
-                setState("attachmentData", attachmentDataContent);
-            };
-            attachmentData.readAsDataURL(newinput.files[0]);
-
-            // Set virtual form to state
-            setState("attachment", newinput.files[0]);
-            
-          }, 'image/jpeg', config.attachments.quality); 
-        }
-      }
-    }
-
-    // Resize attachment file
-    let resizeImage = (img) => {
-
-      let max_width = config.attachments.maxWidth;
-      let max_height = config.attachments.maxHeight;
-
-      let canvas = document.createElement('canvas');
-
-      let width = img.width;
-      let height = img.height;
-
-      // Calculate the width and height, constraining the proportions
-      if (width > height) {
-        if (width > max_width) {
-
-          height = Math.round(height *= max_width / width);
-          width = max_width;
-
-        }
-      } 
-      else {
-        if (height > max_height) {
-
-          width = Math.round(width *= max_height / height);
-          height = max_height;
-
-        }
-      }
-
-      // Resize the canvas and draw the image data into it
-      canvas.width = width;
-      canvas.height = height;
-      let ctx = canvas.getContext("2d");
-      ctx.drawImage(img, 0, 0, width, height);
-
-      return canvas;
     }
 });
